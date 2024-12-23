da01e02aa3073572f1bd375acd237450a1dcbf43
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-12-23 12:01:47 +0100

    modified get jobs logit and add try  and error handling

---
+++ get_jobs_data_validator.py
@@ -0,0 +1,36 @@
+import requests
+from api_get_token import GetToken
+
+
+class GetJobs:
+
+    def __init__(self):
+        self.token = GetToken().get_token()
+        self.route = "jobs/get/"
+        self.base_url = "https://api.laurentiumarian.ro/"
+
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token}"
+        }
+
+    def get_jobs(self, company: str):
+        url = f"{self.base_url}{self.route}?company={company}"
+
+        try:
+            response = requests.request("GET", url, headers=self.headers)
+            response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        except requests.exceptions.RequestException as e:
+            # Catch any exceptions raised by the `requests` library
+            raise Exception(
+                f"Error while fetching jobs for company {company}: {e}")
+
+        try:
+            return response.json()
+        except ValueError:
+            # Handle cases where the response is not valid JSON
+            raise Exception(
+                f"Failed to parse JSON response for company {company}")
+
+
+
