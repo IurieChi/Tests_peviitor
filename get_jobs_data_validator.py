cbdbbc3b861fcb949c92f78bcd744b570aa8bb53
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2025-01-10 16:21:44 +0100

    updated function get jobs to extract all jobs from API if more pages are available

--- get_jobs_data_validator.py
+++ get_jobs_data_validator.py
@@ -6,8 +6,10 @@ class GetJobs:
 
     def __init__(self):
         self.token = GetToken().get_token()
-        self.route = "jobs/get/"
         self.base_url = "https://api.laurentiumarian.ro/"
+        self.route = "jobs/get/"
+
+        # payload = {"company": f"{company}"}
 
         self.headers = {
             "Content-Type": "application/json",
@@ -27,22 +29,38 @@ class GetJobs:
         Returns:
             Json: Data with jobs
         """
-        url = f"{self.base_url}{self.route}?company={company}"
+
+        page = 1
+        all_jobs = []
+
+        while True:
+            try:
+                response = requests.request("GET", url=f"{self.base_url}{self.route}?company={company}&page={page}", headers=self.headers)
+
+                # Raise an HTTPError for bad responses (4xx and 5xx)
+                response.raise_for_status()
+
+                response = response.json()
+
+                # Add jobs to the jobs
+                jobs = response['results']
+                all_jobs.extend(jobs)
+
+                next_page = response['next']
+
+                if next_page is None:
+                    break
+                # Incriment page and continue to extract all jobs from API
+                page += 1
+
+            except requests.exceptions.RequestException as e:
+                # Catch any exceptions raised by the `requests` library
+                raise Exception(
+                    f"Error while fetching jobs for company {company}: {e}")
 
         try:
-            response = requests.request("GET", url, headers=self.headers)
-            response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
-        except requests.exceptions.RequestException as e:
-            # Catch any exceptions raised by the `requests` library
-            raise Exception(
-                f"Error while fetching jobs for company {company}: {e}")
-
-        try:
-            return response.json()
+            return all_jobs
         except ValueError:
             # Handle cases where the response is not valid JSON
             raise Exception(
                 f"Failed to parse JSON response for company {company}")
-
-
-
