9b1819d8afface4a96c3ae659acdbcf82dff7727
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2025-01-23 15:10:40 +0100

    updated api_get_token

--- api_get_token.py
+++ api_get_token.py
@@ -1,34 +1,49 @@
+"""Get tocket from API
+
+Raises:
+    Exception: _description_
+    Exception: _description_
+
+Returns:
+    _type_: Token
+"""
 import requests
 
 class GetToken:
-    
     """
     - Method to get token
     """
-    
     def __init__(self):
-        
+
         self.EMAIL = 'chigaiiura@yahoo.com'
         self.DOMAIN = 'https://api.peviitor.ro/v5/'
         self.TOKEN_ROUTE = 'get_token/'
-        
         self.header = {
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+            'user-agent': 
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
             }
-        
-    
+
     def get_token(self):
+        """_summary_
+
+        Raises:
+            Exception: Raise HTTPError for bad responses (4xx and 5xx)
+            Exception: Error while attempting to get token:
+
+        Returns:
+            _type_: Token Key
+        """
         url = f"{self.DOMAIN}{self.TOKEN_ROUTE}"
-        
+
         try:
-            response = requests.post(url, json={"email": self.EMAIL}, headers=self.header)
+            response = requests.post(url, json={"email": self.EMAIL},headers=self.header,timeout=10)
             response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
         except requests.exceptions.RequestException as e:
             # Catch any requests-related exceptions and raise a new exception with details
-            raise Exception(f"Error while attempting to get token: {e}")
-        
+            raise requests.exceptions.RequestException("Error while attempting to get token:")from e
+
         try:
             return response.json()["access"]
-        except KeyError:
+        except KeyError as e:
             # Handle case where "access" key is not in the response
-            raise Exception("Token not found in the response")
+            raise KeyError ("'access' not found in the response")from e
