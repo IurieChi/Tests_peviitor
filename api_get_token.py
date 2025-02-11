b0aab9fdede229cb71128f3674ea724145cf125a
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2025-02-11 10:30:21 +0100

    formated code accordingly pep

--- api_get_token.py
+++ api_get_token.py
@@ -7,21 +7,23 @@ Raises:
 Returns:
     _type_: Token
 """
+
 import requests
+
 
 class GetToken:
     """
     - Method to get token
     """
+
     def __init__(self):
 
-        self.EMAIL = 'chigaiiura@yahoo.com'
-        self.DOMAIN = 'https://api.peviitor.ro/v5/'
-        self.TOKEN_ROUTE = 'get_token/'
+        self.EMAIL = "chigaiiura@yahoo.com"
+        self.DOMAIN = "https://api.peviitor.ro/v5/"
+        self.TOKEN_ROUTE = "get_token/"
         self.header = {
-            'user-agent': 
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
-            }
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+        }
 
     def get_token(self):
         """_summary_
@@ -36,14 +38,18 @@ class GetToken:
         url = f"{self.DOMAIN}{self.TOKEN_ROUTE}"
 
         try:
-            response = requests.post(url, json={"email": self.EMAIL},headers=self.header,timeout=10)
+            response = requests.post(
+                url, json={"email": self.EMAIL}, headers=self.header, timeout=10
+            )
             response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
         except requests.exceptions.RequestException as e:
             # Catch any requests-related exceptions and raise a new exception with details
-            raise requests.exceptions.RequestException("Error while attempting to get token:")from e
+            raise requests.exceptions.RequestException(
+                "Error while attempting to get token:"
+            ) from e
 
         try:
             return response.json()["access"]
         except KeyError as e:
             # Handle case where "access" key is not in the response
-            raise KeyError ("'access' not found in the response")from e
+            raise KeyError("'access' not found in the response") from e
