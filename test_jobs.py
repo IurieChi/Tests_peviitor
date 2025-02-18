8639aa21767d3756a4b92df415bf3a2c59d0761a
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2025-02-18 11:51:12 +0100

    updated logic and code

--- test_jobs.py
+++ test_jobs.py
@@ -1,40 +1,50 @@
+"""_summary_
+
+Raises:
+    Exception: _description_
+
+Returns:
+    _type_: False or True for jobs if job pass all validation
+"""
+
 import requests
-import subprocess
+from job_location import validate_city
 
 
-class ValidateJob():
+class ValidateJob:
+    """
+    Logic to validate all jobs
+    """
 
-    def __init__(self, job):
+    default_header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+    }
+
+    def __init__(self, job, header=None):
         self.job = job
+        self.header = header if header is not None else ValidateJob.default_header
 
     def get_content(self):
         """
         Extract job content fetched from the  job URL.
 
         """
-        # content = requests.get(self.job["job_link"], timeout=10)
-        # return content
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_8_8; like Mac OS X) AppleWebKit/535.14 (KHTML, like Gecko) Chrome/49.0.3028.253 Mobile Safari/603.0',
-        }
 
         try:
-            # Construct the curl command
-            command = [
-                'curl', '-s', '-A', headers['User-Agent'], self.job["job_link"]
-            ]
-            # Execute the curl command and capture the output
-            result = subprocess.run(
-                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=False)
-            # Check if there was an error
-            if result.returncode != 0:
-                raise Exception(result.stderr.decode('utf-8'))
-            # Decode the output using utf-8
-            return result.stdout.decode('utf-8')
-        except Exception as e:
+            response = requests.get(
+                url=self.job["job_link"],
+                headers=self.header,
+                allow_redirects=True,
+                timeout=50,
+            )
+            if response.status_code == 200:
+                return response.text
+
+        except requests.RequestException as e:
             # Handle exceptions (e.g., network errors, invalid responses)
             print(f"An error occurred: {e}")
-            return None
 
     def validate_job_link(self):
         """Check if the link is accessible.
@@ -43,12 +53,23 @@ class ValidateJob():
 
         """
 
-        response = requests.get(self.job["job_link"], timeout=10)
         try:
-            assert response.status_code == 200, f"Company: {self.job["company_name"]}: {
+            response = requests.get(
+                url=self.job["job_link"],
+                headers=self.header,
+                allow_redirects=True,
+                timeout=20,
+            )
+
+            # Assert the status code is 200
+            assert (
+                response.status_code == 200
+            ), f"Company: {self.job["company_name"]}: {
                 self.job["job_link"]} Link is not accessible"
 
-            return self.job
+        except requests.RequestException as e:
+            # Handle exceptions (e.g., network errors, invalid responses)
+            print(f"An error occurred: {e}")
 
         except AssertionError as e:
             print(f"AssertionError: {e}")
@@ -65,7 +86,9 @@ class ValidateJob():
 
         """
         try:
-            assert self.job["job_title"] in content, f"Job title not found in the Page {
+            assert (
+                self.job["job_title"] in content
+            ), f"Job title:'{self.job["job_title"]}' NOT FOUND on the page:{
                 self.job["job_link"]}"
             return self.job
 
@@ -74,7 +97,7 @@ class ValidateJob():
             self.job["job_title"] = False
             return self.job
 
-    def validate_job_location(self):
+    def validate_job_city(self):
         """
         Validate that the 'city' field is not an empty string or an empty list.
 
@@ -82,8 +105,30 @@ class ValidateJob():
             AssertionError: If 'city' is an empty string or an empty list.
         """
         try:
-            assert self.job["city"] not in ("", []), f"City fild is empty for Company: {
+
+            assert self.job["city"] not in (
+                "",
+                [],
+            ), f"City fild is empty for Company: {
                 self.job["company_name"]} and job title: {self.job["job_title"]}"
+
+            validated_cities = validate_city(self.job["city"])
+
+            if isinstance(validated_cities, list):
+                assert all(validated_cities), (
+                    f"One or more cities in the list are not valid for Company: "
+                    f"{
+                    self.job['company_name'].upper()}: and job title: {self.job['job_title']}"
+                )
+                self.job["city"] = validated_cities
+                # print(self.job["city"])
+            else:
+                assert validated_cities, (
+                    f"{self.job['city']} is not a valid city in Romania for Company: "
+                    f"{
+                                          self.job['company_name']} and job title: {self.job['job_title']}"
+                )
+                # self.job["city"] = validated_cities
 
         except AssertionError as e:
 
@@ -98,9 +143,19 @@ class ValidateJob():
         Raises:
             AssertionError: If 'remote' is an empty string or an empty list.
         """
+        expected_job_type_formats = ["hybrid", "remote", "on-site"]
         try:
-            assert self.job["remote"] not in ("", []), f"Job type fild is empty for Company: {
+            assert self.job["remote"] not in (
+                "",
+                [],
+            ), f"Job type fild is empty for Company: {
                 self.job["company_name"]} and job title: {self.job["job_title"]}"
+            if isinstance(self.job["remote"], list):
+                for job in self.job["remote"]:
+                    assert (
+                        job in expected_job_type_formats
+                    ), f"Job type {self.job["remote"]
+                        } is not like expected format for job title: {self.job["job_title"]}"
 
         except AssertionError as e:
 
